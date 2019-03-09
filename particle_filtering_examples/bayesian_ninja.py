91b0200c5ce0325c93540aa489d35206fcb08c01
Christopher Claxton <c.d.claxton@gmail.com>
2019-03-09 21:32:05 +0000

    Updated the line style.

--- particle_filtering_examples/bayesian_ninja.py
+++ particle_filtering_examples/bayesian_ninja.py
@@ -200,8 +200,8 @@ if __name__ == '__main__':
         plt.plot(np.repeat(t, N), x_particles[:, t], 'b.', alpha=0.2)
 
     plt.title("Position of the quail as a function of time")
-    plt.plot(t_range, x, '--gx', label="actual")
-    plt.plot(t_range, x_est, 'r:o', label="estimated")
+    plt.plot(t_range, x, '--go', label="actual")
+    plt.plot(t_range, x_est, 'b:x', label="estimated")
     plt.xlabel("Time index")
     plt.ylabel("Position")
     plt.legend()
